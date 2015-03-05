a0dd88283429660af71ec2fbbf81166dd4453329
otamm <oivatom@gmail.com>
2015-03-05 11:53:12 -0300

    Finished tests for first method

--- spec/r_math_plus_spec.rb
+++ spec/r_math_plus_spec.rb
@@ -16,9 +16,20 @@ describe RMathPlus do
       (RMathPlus.is_prime?(4)).should == false
     end
 
-    #it "returns true when a big prime number is passed as param" do
-    #  expect(RMathPlus.is_prime?(104729)).to == true
-    #end
+    it "returns true when a big prime number is passed as param" do
+      (RMathPlus.is_prime?(104729)).should == true
+    end
+
+    it "executes faster when an array is passed as second parameter" do
+      begin1 = Time.now
+        (RMathPlus.is_prime?(50,[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]))
+      finish1 = Time.now
+
+      begin2 = Time.now
+        (RMathPlus.is_prime?(50))
+      finish2 = Time.now
+      ((finish1 - begin1) > (finish2 - begin2)).should == true
+    end
 
   end
 
